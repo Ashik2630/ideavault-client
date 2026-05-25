acc5aa868a769c9a775056e1c321a632139f1872
Ashik2630 <ashik426370@gmail.com>
2026-05-25 13:25:58 +0600

    addad redme.md file

--- README.md
+++ README.md
@@ -1,36 +1,108 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# IdeaVault – Startup Idea Sharing Platform
 
-## Getting Started
+ A modern full-stack startup idea sharing platform where users can explore innovative startup concepts, interact through comments, and collaborate with the community to validate ideas.
 
-First, run the development server:
+## 🌐 Live Website
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+IdeaVault Live Site:
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+🚀 Features
+🔐 JWT Authentication with Email/Password & Google Login
+💡 Users can create, update, and delete startup ideas
+💬 Interactive comment system with edit & delete functionality
+🔍 Search ideas by title and filter by category
+🌙 Dark & Light theme toggle from navbar
+📱 Fully responsive for Mobile, Tablet, and Desktop
+⚡ Protected private routes with persistent login
+🎯 Trending Ideas section on homepage
+🧑 User profile management system
+🔔 Toast notifications for all CRUD operations and authentication
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+ ### 🛠️ Tech Stack:
+Frontend
+Next.js
+React.js
+Tailwind CSS
+HeroUI / DaisyUI
+Firebase Authentication
+Axios
+React Hot Toast
+Backend
+Node.js
+Express.js
+MongoDB
+JWT Authentication
+CORS
+dotenv
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+### 🔒 Authentication Features:
+Email & Password Login
+Google Authentication
+JWT Token Generation
+Protected Routes
+Persistent User Session
+Secure API Authorization
 
-## Learn More
+### 📌 Core Functionalities:
+🏠 Home Page
+Banner Slider
+Trending Ideas Section
+Extra Creative Sections
+CTA Buttons
+💡 Ideas Page
+All ideas in responsive grid layout
+Search by title
+Category filter
+View Details functionality
+📝 Add Idea
 
-To learn more about Next.js, take a look at the following resources:
+Authenticated users can:
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+Share startup ideas
+Add descriptions, categories, tags, and budget
+Upload idea images
+💬 Comment System
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
+### Users can:
 
-## Deploy on Vercel
+Add comments
+Edit own comments
+Delete own comments
+👤 My Ideas
+View personal ideas
+Update ideas using modal
+Delete ideas with confirmation modal
+📊 My Interactions
+View commented ideas
+Track engagement activity
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+### 🎨 UI/UX Highlights:
+Modern responsive design
+Clean typography & spacing
+Equal card sizing
+Custom loading spinner
+Beautiful toast notifications
+Custom 404 page
+Dark & Light mode support
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+### NPM Packages Used:
+# Client
+next
+react
+tailwindcss
+Bather Auth
+react-hot-toast
+swiper
+lucide-react
+
+# Server
+express
+mongodb
+jsonwebtoken
+cors
+dotenv
+
+
+👨‍💻 Developer
+
+Developed by Md. Ashikur Rahman
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { authClient } from "@/lib/auth-client";
 import { Check, EyeSlash } from "@gravity-ui/icons";
 import {
@@ -22,8 +23,10 @@ import { IoMdLogIn } from "react-icons/io";
 
 const LoginPage = () => {
   const [isVisible, setIsVisible] = useState(false);
+
   const router = useRouter();
   const searchParams = useSearchParams();
+
   const callbackUrl = searchParams.get("callbackUrl") || "/";
 
   const onSubmit = async (e) => {
@@ -50,39 +53,50 @@ const LoginPage = () => {
   };
 
   const handleGoogleLoginIn = async () => {
-   const data = await authClient.signIn.social({
+    await authClient.signIn.social({
       provider: "google",
       callbackURL: callbackUrl,
     });
-    if (data) {
-      toast.success("Logged in successfully");
-      router.replace(callbackUrl);
-      return;
-    }
   };
 
   return (
-    <Card className="mx-5 md:mx-auto md:w-125 py-10 my-30 rounded hover: translate-0 scale-1.5">
+    <Card className="mx-5 md:mx-auto md:w-125 py-10 my-30 rounded-2xl shadow-xl">
       <div className="text-center space-y-1">
-        <p className=" flex justify-center text-4xl mb-5"> <IoMdLogIn /></p>
-        <h1 className="text-center text-2xl font-bold">Welcome Back</h1>
-        <p className="text-gray-500">Login to your Ideas Platform account</p>
+        <p className="flex justify-center text-4xl mb-5 text-[#155dfc]">
+          <IoMdLogIn />
+        </p>
+
+        <h1 className="text-center text-2xl font-bold">
+          Welcome Back
+        </h1>
+
+        <p className="text-gray-500">
+          Login to your Ideas Platform account
+        </p>
       </div>
 
-      <Form className="flex w-96 mx-auto flex-col gap-4" onSubmit={onSubmit}>
+      <Form
+        className="flex w-full md:w-96 mx-auto flex-col gap-4 mt-6"
+        onSubmit={onSubmit}
+      >
         <TextField
           isRequired
           name="email"
           type="email"
           validate={(value) => {
-            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+            if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)
+            ) {
               return "Please enter a valid email address";
             }
+
             return null;
           }}
         >
           <Label>Email</Label>
+
           <Input placeholder="Enter your Email" />
+
           <FieldError />
         </TextField>
 
@@ -95,9 +109,11 @@ const LoginPage = () => {
             if (value.length < 8) {
               return "Password must be at least 8 characters";
             }
+
             if (!/[A-Z]/.test(value)) {
               return "Password must contain at least one uppercase letter";
             }
+
             if (!/[0-9]/.test(value)) {
               return "Password must contain at least one number";
             }
@@ -106,20 +122,26 @@ const LoginPage = () => {
           }}
         >
           <Label>Password</Label>
+
           <InputGroup>
             <InputGroup.Input
-              className="w-full max-w-70 relative"
-              placeholder="Enter your Email"
+              className="w-full relative"
+              placeholder="Enter your Password"
               type={isVisible ? "text" : "password"}
             />
+
             <InputGroup.Suffix className="pr-0">
               <Button
                 isIconOnly
-                aria-label={isVisible ? "Hide password" : "Show password"}
+                aria-label={
+                  isVisible
+                    ? "Hide password"
+                    : "Show password"
+                }
                 size="sm"
                 variant="ghost"
                 onPress={() => setIsVisible(!isVisible)}
-                className=" absolute right-12 md:right-17"
+                className="absolute right-2"
               >
                 {isVisible ? (
                   <Eye className="size-4" />
@@ -129,39 +151,51 @@ const LoginPage = () => {
               </Button>
             </InputGroup.Suffix>
           </InputGroup>
+
           <Description>
             Must be at least 8 characters with 1 uppercase and 1 number
           </Description>
+
           <FieldError />
         </TextField>
 
         <div className="flex gap-2">
           <Button
             type="submit"
-            className="bg-[#155dfc] w-full hover:bg-[#155dfc] transition"
+            className="bg-[#155dfc] w-full text-white hover:bg-blue-700 transition"
           >
             <Check />
             Login
           </Button>
-          <Button type="reset" variant="secondary" className="text-[#155dfc]">
+
+          <Button
+            type="reset"
+            variant="secondary"
+            className="text-[#155dfc]"
+          >
             Reset
           </Button>
         </div>
       </Form>
+
       {/* Divider */}
       <div className="flex items-center my-4">
         <div className="grow h-px bg-gray-300"></div>
-        <span className="mx-3 text-sm text-gray-500">or continue with</span>
+
+        <span className="mx-3 text-sm text-gray-500">
+          or continue with
+        </span>
+
         <div className="grow h-px bg-gray-300"></div>
       </div>
 
       {/* Google Button */}
       <button
         onClick={handleGoogleLoginIn}
-        className="w-full flex items-center justify-center gap-3 border border-gray-300 rounded-lg py-2.5 bg-[#155dfc]  hover:bg-[#155dfc] transition"
+        className="w-full flex items-center justify-center gap-3 border border-gray-300 rounded-lg py-2.5 bg-[#155dfc] hover:bg-blue-700 transition"
       >
-        {/* Google Icon */}
         <FcGoogle className="text-xl" />
+
         <span className="text-sm font-medium text-white">
           Sign in with Google
         </span>
